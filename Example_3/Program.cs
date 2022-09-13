d1798e807cdc06f58c286e8ddf61ff9ea1179370
Elena <vodopyan.elena@gmail.com>
2022-09-13 12:52:09 +0300

    example_3

---
+++ Example_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число, от 1 до которого вам необходимо рассчитать кубы");
+int numberN = Convert.ToInt32(Console.ReadLine());
+CubNumber(numberN);
+
+void CubNumber(int numberN)
+{
+    int index = 1;
+    while (index <= numberN)
+    {
+        Console.WriteLine($"{index}^3 = {Math.Pow(index, 3)}.");
+        index++;
+    }
+}
